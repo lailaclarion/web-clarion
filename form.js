7b224bfc8109c208d1f5bdef167f67ffac06659d
Clarion <lailaclarion@gmail.com>
2026-04-10 13:46:01 +0700

    Update tracking tag

--- form.js
+++ form.js
@@ -2,7 +2,7 @@ document.getElementById('clarionForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const scriptURL = 'https://script.google.com/macros/s/AKfycbxSrWtR7ViBrmAgjzayuTkcCXkGBnCyA_ByhtUwK1WRVl0sZp-YNb-zNuB8-eIkxKORoQ/exec';
-    
+
     const nama = document.getElementById('nama').value;
     const wa = document.getElementById('wa').value;
     const kota = document.getElementById('kota').value;
@@ -20,16 +20,35 @@ document.getElementById('clarionForm').addEventListener('submit', function(e) {
     params.append('tanggal', new Date().toLocaleString());
     params.append('source', 'Homepage');
 
-    const nomorAdmin = "6281378699699"; 
-    const teksPesan = `Halo Clarion Indonesia, saya ingin konsultasi:%0A%0A*Nama:* ${nama}%0A*WhatsApp:* ${wa}%0A*Kota:* ${kota}%0A*Sumber Air:* ${sumber}%0A*Estimasi Budget:* ${budget}%0A*Keluhan:* ${pesan}`;
-    
+    const nomorAdmin = "6281378699699";
+
+    const teksPesan =
+`Halo Clarion Indonesia, saya ingin konsultasi:%0A%0A
+Nama: ${nama}%0A
+WhatsApp: ${wa}%0A
+Kota: ${kota}%0A
+Sumber Air: ${sumber}%0A
+Estimasi Budget: ${budget}%0A
+Keluhan: ${pesan}`;
+
     const urlWA = `https://wa.me/${nomorAdmin}?text=${teksPesan}`;
 
-    fetch(scriptURL, { 
-        method: 'POST', 
+    // GOOGLE ADS CONVERSION
+    gtag('event', 'conversion', {
+        'send_to': 'AW-18065186210/SsbiCOOX15YcEKK7k6ZD',
+        'value': 1.0,
+        'currency': 'IDR'
+    });
+
+    // SEND TO GOOGLE SHEET
+    fetch(scriptURL, {
+        method: 'POST',
         body: params,
-        mode: 'no-cors' 
-    })
-    .then(() => window.location.assign(urlWA))
-    .catch(() => window.location.assign(urlWA));
+        mode: 'no-cors'
+    });
+
+    // REDIRECT WA
+    setTimeout(() => {
+        window.location.href = urlWA;
+    }, 300);
 });
